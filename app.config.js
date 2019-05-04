61f92dc6e539227d33d5408b0f8825331ae8f9ab
song <songten@icloud.com>
2019-05-05 07:19:12 +0800

    test the lint-staged3

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     ak: 'x9-CRGH5Rf2DVxTjdmORq8xk1esrwcMbxEwOtSFy',
     sk: 'Pm-6EATNNawjss5CgRwt_enmX08Xs8_JM9u9g70p',
     bucket: 'admin',
-    host: 'http://pfk4b2cs8.bkt.clouddn.com/',
+    host: 'http://pfk4b2cs8.bkt.clouddn.com/'
     qwe: ''
   }
 }
