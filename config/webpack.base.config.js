a51658cfc3d8ca556fb069a0a7f37bc614df2e0b
song <songten@icloud.com>
2019-05-07 05:31:39 +0800

    done

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,25 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const OfflinePlugin = require('offline-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OfflinePlugin = require("offline-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-
-const ROOTPATH = path.resolve(__dirname)
-const isProd = process.env.NODE_ENV === 'production';
+const ROOTPATH = path.resolve(__dirname);
+const isProd = process.env.NODE_ENV === "production";
 
 const baseWebpackConfig = {
-  mode: isProd ? 'production' : 'development',
+  mode: isProd ? "production" : "development",
   entry: {
-    app: path.join(ROOTPATH, '../src/index'),
+    app: path.join(ROOTPATH, "../src/index")
   },
   output: {
-    path: path.join(ROOTPATH, '../dist'),
+    path: path.join(ROOTPATH, "../dist")
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: [".js", ".jsx", ".json"]
   },
 
   // mode为production自动启用
@@ -27,20 +26,24 @@ const baseWebpackConfig = {
     minimizer: [
       new UglifyJsPlugin({
         uglifyOptions: {
+          warnings: false,
           comments: false,
           ecma: 6,
           cache: true,
           parallel: true,
-        },
+          compress: {
+            drop_console: true, // console
+            pure_funcs: ["console.log"] // 移除console
+          }
+        }
       }),
       // 压缩css
-      new OptimizeCSSAssetsPlugin(
-        // {
-        // cssProcessorOptions: {
-        //   map: { inline: false }
-        // }
-        // }
-      ),
+      new OptimizeCSSAssetsPlugin()
+      // {
+      // cssProcessorOptions: {
+      //   map: { inline: false }
+      // }
+      // }
     ],
     runtimeChunk: true,
     namedModules: true,
@@ -63,40 +66,40 @@ const baseWebpackConfig = {
           //   return module.context && module.context.includes('node_modules');
           // },
           test: /[\\/]node_modules[\\/]/,
-          chunks: 'all',
-          name: 'vendor',
+          chunks: "all",
+          name: "vendor",
           priority: 10, // 优先
-          enforce: true,
-        },
-      },
-    },
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
     // 由于mac不区分大小写，linux区分大小写，可能导致mac上正常，在部署时出错，所以强制区分大小写
     new CaseSensitivePathsPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.join(ROOTPATH, '../src/index.html'),
+      filename: "index.html",
+      template: path.join(ROOTPATH, "../src/index.html")
     }),
     // PWA
     new OfflinePlugin(),
     new WebpackPwaManifest({
-      name: 'My Progressive Web App',
-      short_name: 'MyPWA',
-      description: 'My awesome Progressive Web App!',
-      background_color: '#ffffff',
+      name: "My Progressive Web App",
+      short_name: "MyPWA",
+      description: "My awesome Progressive Web App!",
+      background_color: "#ffffff",
       icons: [
         {
-          src: path.resolve('src/resource/assets/logo.jpg'),
-          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          src: path.resolve("src/resource/assets/logo.jpg"),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
         },
         {
-          src: path.resolve('src/resource/assets/logo.jpg'),
-          size: '1024x1024', // manifest default
-        },
-      ],
-    }),
-  ],
-}
+          src: path.resolve("src/resource/assets/logo.jpg"),
+          size: "1024x1024" // manifest default
+        }
+      ]
+    })
+  ]
+};
 
 module.exports = baseWebpackConfig;
