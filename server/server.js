528c0f6ee4dd4faeaacd2f6910ff56273a493fe9
songtianen <songtianen@sina.com>
2020-04-14 12:37:19 +0800

    delete gz plugin

--- server/server.js
+++ server/server.js
@@ -4,16 +4,15 @@ const favicon = require('serve-favicon'); // 引用标签页图标
 const bodyParser = require('body-parser'); // 转化 请求body 为 json格式的数据
 const router = require('./routes/index.js');
 // const expressStaticGzip = require('express-static-gzip');
-// const compression = require('compression');
+const compression = require('compression');
 
 const isEnv = process.env.NODE_ENV;
 const PORT = isEnv === 'production' ? 8888 : 6666;
 let app = express();
-
+app.use(compression()); // 注册资源压缩的中间件
 app.use(bodyParser.json()); // 请求体 json格式的数据转换成 req.body 格式的数据
 app.use(bodyParser.urlencoded({ extended: false })); // form data 格式转换 req.body 格式
 app.use(favicon(path.join(__dirname, '../favicon.ico'))); // 浏览器标签页的图标
-// app.use(compression()); // 注册资源压缩的中间件
 
 if (isEnv === 'production') {
   // ----------------------
