a31fbe9626c7d00282e73dece3abda0794db1da9
songtianen <songtianen@sina.com>
2020-04-14 13:07:26 +0800

    add gzip

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const merge = require('webpack-merge');
 // const PurifyCSS = require("purifycss-webpack");
 // const glob = require("glob-all");
-// const CompressionPlugin = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.config');
@@ -128,14 +128,14 @@ const webpackProdConfig = merge(baseWebpackConfig, {
     //   ])
     // }),
     //  前端压缩还是后端压缩商量好
-    // new CompressionPlugin({
-    //   filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
-    //   algorithm: 'gzip', // 算法
-    //   test: /\.(js|css)$/, // 压缩 js 与 css
-    //   threshold: 10240, // 只处理比这个值大的资源。按字节计算
-    //   minRatio: 0.8, // 只有压缩率比这个值小的资源才会被处理
-    //   deleteOriginalAssets: true,
-    // }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+      algorithm: 'gzip', // 算法
+      test: /\.(js|css)$/, // 压缩 js 与 css
+      threshold: 10240, // 只处理比这个值大的资源。按字节计算
+      minRatio: 0.8, // 只有压缩率比这个值小的资源才会被处理
+      deleteOriginalAssets: true,
+    }),
   ],
 });
 
